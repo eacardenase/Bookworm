698a5387a353e61fe7de24a7a592a2376e872bb1
Edwin Cardenas <eacardenase@gmail.com>
2026-04-30 17:33:16 -0500

    feat: introducing TextEditor

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -7,41 +7,15 @@
 
 import SwiftUI
 
-struct PushButton: View {
-    let title: String
-    @Binding var isOn: Bool
-
-    var onColors: [Color] = [.red, .yellow]
-    var offColors: [Color] = [.white.opacity(0.6), .white.opacity(0.4)]
-
-    var body: some View {
-        Button(title) {
-            isOn.toggle()
-        }
-        .padding()
-
-        .background(
-            LinearGradient(
-                colors: isOn ? onColors : offColors,
-                startPoint: .top,
-                endPoint: .bottom
-            )
-        )
-        .clipShape(.capsule)
-        .shadow(radius: isOn ? 0 : 5)
-    }
-}
-
 struct ContentView: View {
-    @State private var rememberMe = false
+    @AppStorage("notes") private var notes = ""
 
     var body: some View {
-        VStack {
-            PushButton(title: "Remember Me", isOn: $rememberMe)
-
-            Text(rememberMe ? "On" : "Off")
+        NavigationStack {
+            TextEditor(text: $notes)
+                .navigationTitle("Notes")
+                .padding()
         }
-        .animation(.easeInOut, value: rememberMe)
     }
 }
 
