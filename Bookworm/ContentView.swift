cb874bb4dff5937a7641e8b84b028290c45e3f3a
Edwin Cardenas <eacardenase@gmail.com>
2026-04-30 17:29:22 -0500

    feat: two-way binding between views

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -7,15 +7,41 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct PushButton: View {
+    let title: String
+    @Binding var isOn: Bool
+
+    var onColors: [Color] = [.red, .yellow]
+    var offColors: [Color] = [.white.opacity(0.6), .white.opacity(0.4)]
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Button(title) {
+            isOn.toggle()
         }
         .padding()
+
+        .background(
+            LinearGradient(
+                colors: isOn ? onColors : offColors,
+                startPoint: .top,
+                endPoint: .bottom
+            )
+        )
+        .clipShape(.capsule)
+        .shadow(radius: isOn ? 0 : 5)
+    }
+}
+
+struct ContentView: View {
+    @State private var rememberMe = false
+
+    var body: some View {
+        VStack {
+            PushButton(title: "Remember Me", isOn: $rememberMe)
+
+            Text(rememberMe ? "On" : "Off")
+        }
+        .animation(.easeInOut, value: rememberMe)
     }
 }
 
