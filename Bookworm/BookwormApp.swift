a75152708f57792bfa6105abcf491421501e93bb
Edwin Cardenas <eacardenase@gmail.com>
2026-04-30 17:13:02 -0500

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Edwin Cardenas on 4/30/26.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
